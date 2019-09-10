4a0011809e6f16d3915399f7185988bee2f6f3bb
Jan Verweij <jan.verweij@liferay.com>
2019-09-10 11:13:13 +0200

    support multiple categories and endpoint for specific vocabulary

--- src/main/java/custom/rest/api/application/CustomRestApiApplication.java
+++ src/main/java/custom/rest/api/application/CustomRestApiApplication.java
@@ -12,6 +12,8 @@ import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetTagLocalService;
 import com.liferay.asset.kernel.service.AssetTagLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -122,7 +124,7 @@ public class CustomRestApiApplication extends Application {
 	@GET
 	@Path("/categories")
 	@Produces("text/plain")
-	public String categories(@QueryParam("categoryId") long categoryId) throws PortalException {
+	public String getCategories(@QueryParam("categoryId") List<Long> categoryId) {
 		System.out.println("Trying to fetch the categories...");
 
 		String result = "[";
@@ -130,12 +132,12 @@ public class CustomRestApiApplication extends Application {
 		// get distinct values
 		Set<String> temp = new HashSet<String>();
 
-		//AssetCategory category = AssetCategoryLocalServiceUtil.getCategory(categoryId);
-		List<AssetCategory> categories = AssetCategoryLocalServiceUtil.getChildCategories(categoryId);
-
-		//List<AssetTag> tags = AssetTagLocalServiceUtil.getGroupTags(groupId);
-		for (AssetCategory category : categories) {
-			temp.add(category.getName());
+		List<AssetCategory> categories;
+		for(Long catId : categoryId) {
+			categories = AssetCategoryLocalServiceUtil.getChildCategories(catId);
+			for (AssetCategory category : categories) {
+				temp.add(category.getName());
+			}
 		}
 
 		for (String category : temp) {
@@ -145,6 +147,50 @@ public class CustomRestApiApplication extends Application {
 		result = result.substring(0,result.lastIndexOf(',')) + "]";
 
 		System.out.println(result);
+
+		return result;
+	}
+
+	//http://localhost:8080/o/custom/categories?categoryId=43291
+	@GET
+	@Path("/vocabulary")
+	@Produces("text/plain")
+	public String getVocabulary(@QueryParam("vocabularyId") long vocabularyId) {
+		System.out.println("Trying to fetch the vocabulary...");
+
+		String result = "[";
+
+		OrderByComparator comparator = OrderByComparatorFactoryUtil.create("AssetCategory", "name",true);
+		List<AssetCategory> categories = AssetCategoryLocalServiceUtil.getVocabularyCategories(vocabularyId,0,999,comparator);
+
+		// get distinct values
+		Set<String> temp = new HashSet<String>();
+
+		for (AssetCategory category : categories) {
+			temp.add(getCategoryPath(category));
+		}
+
+		for (String category : temp) {
+			result += "\"" + category +  "\",";
+		}
+
+		result = result.substring(0,result.lastIndexOf(',')) + "]";
+
+		System.out.println(result);
+
+		return result;
+	}
+
+	//recursive get fullpath
+	private String getCategoryPath(AssetCategory category) {
+		String result = "";
+
+		if (category.isRootCategory()) {
+			result = category.getName();
+		}
+		else {
+			result += getCategoryPath(category.getParentCategory()) +  " > " + category.getName();
+		}
 
 		return result;
 	}
